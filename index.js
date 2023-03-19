ee3fe5db217d42f53593d85daf8e54e688e472a5
Avinash Pandey <pandeyavi200@gmail.com>
2023-03-20 00:02:09 +0530

    modification in index file

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const connects = require('./src/config/db');
+const cors = require('cors');
+const router = require('./src/routes/routes')
+require('dotenv').config();
+
+const app = express();
+// configure bodyParser to parse JSON data
+app.use(bodyParser.json());
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+app.use('/', router)
+
+// start the server
+connects();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
