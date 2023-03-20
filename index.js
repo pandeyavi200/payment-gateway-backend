69837564631b01b1c3bec031946026c477a45ee5
Avinash Pandey <pandeyavi200@gmail.com>
2023-03-20 10:47:24 +0530

    Remove cors

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const app = express();
 // configure bodyParser to parse JSON data
 app.use(bodyParser.json());
 
-const corsOptions = {
-  origin: process.env.ORIGIN_URL,
-  credentials: true,            //access-control-allow-credentials:true
-  optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: process.env.ORIGIN_URL,
+//   credentials: true,            //access-control-allow-credentials:true
+//   optionSuccessStatus: 200
+// }
+app.use(cors());
 app.use('/', router)
 
 // start the server
